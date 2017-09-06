b722f88c4f44087e6d2a599b7b01e2e58dfedaef
namclu <namlu1205@gmail.com>
2017-09-06 11:53:24 +1000

    2.06 feat: add ApiClient and ApiInterface for Retrofit

---
+++ app/src/main/java/com/namclu/android/popularreels/rest/ApiClient.java
@@ -0,0 +1,27 @@
+package com.namclu.android.popularreels.rest;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by namlu on 06-Sep-17.
+ *
+ * To send network requests to an API, use the Retrofit Builder class and
+ * specify the base URL for the service.
+ */
+
+public class ApiClient {
+
+    public static final String BASE_URL = "http://api.themoviedb.org/3/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
---
+++ app/src/main/java/com/namclu/android/popularreels/rest/ApiInterface.java
@@ -0,0 +1,21 @@
+package com.namclu.android.popularreels.rest;
+
+import com.namclu.android.popularreels.MovieResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by namlu on 06-Sep-17.
+ *
+ * The endpoints are defined inside of an interface using special retrofit annotations
+ * to encode details about the parameters and request method. In addition, the return
+ * value is always a parameterized Call<T> object such as Call<MovieResponse>
+ */
+
+public interface ApiInterface {
+
+    @GET("movie/popular")
+    Call<MovieResponse> getPopularMovies(@Query("api_key") String apiKey);
+}
